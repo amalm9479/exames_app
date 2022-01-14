85336e48acf61fe6c5d4668bc32deab80e577d01
amalm9479 <amalm9479@gmail.com>
2022-01-14 17:46:44 +0300

    second

--- exames_app/lib/main.dart
+++ exames_app/lib/main.dart
@@ -9,6 +9,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(),
+      ),
+    );
   }
 }
