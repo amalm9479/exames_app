30660b611c04fe91cab7dacc1b1c5f5e1b050ff9
amalm9479 <amalm9479@gmail.com>
2022-01-15 17:48:05 +0300

    create ielts and toefl 1

---
+++ exames_app/lib/ielts.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+// ignore_for_file: prefer_const_constructors
+
+void main(List<String> args) {
+  runApp(IeltsExames());
+}
+
+class IeltsExames extends StatelessWidget {
+  const IeltsExames({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ exames_app/lib/toefl.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+void main(List<String> args) {
+  runApp(toeflExames());
+}
+
+class toeflExames extends StatelessWidget {
+  const toeflExames({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
